3daad3dceab43cfebe652f9361d30e04815a9524
Abhisheksharma17 <46749639+Abhisheksharma17@users.noreply.github.com>
2019-09-21 15:31:43 +0530

    Add files via upload

---
+++ Staircase.cpp
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n,a,b;;
+    scanf("%d",&n);
+   
+    for(a=1;a<=n;a++){
+        for(b=1;b<=a;b++){
+            printf("#");
+        }
+        printf("\n");
+    }
+    
+
+}
+
